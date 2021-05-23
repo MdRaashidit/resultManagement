ced3dcd246d02daaff0cc3a307f0a809de7fa36a
fullstacksuiit <rahmantanshit@gmail.com>
2021-05-23 17:10:46 +0530

    ::

--- result/settings.py
+++ result/settings.py
@@ -118,12 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
+STATIC_URL = "/static/"
 if DEBUG:
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+    STATICFILES_DIRS = [str(BASE_DIR / "static")]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG:
+    STATIC_ROOT = str(BASE_DIR / "static")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
